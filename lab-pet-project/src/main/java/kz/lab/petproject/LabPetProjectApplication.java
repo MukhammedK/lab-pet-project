972997db8af3ef118588c6d64765f83ef26fa4ec
cnp.az <akylbek.zhumabayev@processing.kz>
2026-01-07 16:05:44 +0500

    init: empty spring boot project

---
+++ lab-pet-project/src/main/java/kz/lab/petproject/LabPetProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.lab.petproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabPetProjectApplication.class, args);
+    }
+
+}
---
+++ lab-pet-project/src/test/java/kz/lab/petproject/LabPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.lab.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
